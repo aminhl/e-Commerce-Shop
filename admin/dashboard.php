7bd8b6740c0228529a71fed23f35acc93e8dc2a9
aminhl <amin.hlel@esprit.tn>
2021-07-26 21:56:49 +0100

    Adding Session & Create Dashboard

---
+++ admin/dashboard.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+if(isset($_SESSION['username'])){
+  include_once 'init.php';
+  include_once $tpl .'footer.php';
+}
+else{
+    echo 'You Are Not Authorized To Visit This Page';
+    header('Location: index.php');
+    exit();
+}
--- admin/includes/templates/footer.php
+++ admin/includes/templates/footer.php
@@ -1,5 +1,5 @@
     <div class="footer">
-        This Is Footer
+
     </div >
     <script src="<?php echo $js;?>js/jquery-1.12.4.min.js"></script>
     <script src="<?php echo $js;?>js/bootstrap.min.js"></script>
