45636925e4da6e189b8b557a1e2f2458a18cb3ca
aminhl <amin.hlel@esprit.tn>
2021-07-28 16:55:20 +0100

    Code Edit Page

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -3,7 +3,6 @@ session_start();
 if(isset($_SESSION['username'])){
   $pageTitle = 'Dashboard';
   include_once 'init.php';
-  print_r($_SESSION);
   include_once $tpl .'footer.php';
 }
 else{
--- admin/members.php
+++ admin/members.php
@@ -7,7 +7,6 @@
  ================================================
  */
 
-
 session_start();
 $pageTitle = 'Members';
 
@@ -19,51 +18,62 @@ if (isset($_SESSION['username'])) {
 
     }
     elseif($do == 'Edit'){ ?>
-        <h1 class="text-center">Edit Member</h1>
-        <form class="form-horizontal">
-            <!--Start Username Filed-->
-        <div class="form-group form-group-lg">
-          <label class="col-sm-2 control-label">Username</label>
-            <div class="col-sm-10 col-md-6">
-                <input class="form-control" type="text" name="username" autocomplete="off ">
-            </div>
-        </div>
-            <!--End Username Filed-->
-            <!--Start Password Filed-->
-            <div class="form-group form-group-lg">
-                <label class="col-sm-2 control-label">Password</label>
-                <div class="col-sm-10 col-md-6">
-                    <input class="form-control" type="password" name="password" autocomplete="new-password">
-                </div>
-            </div>
-            <!--End Password Filed-->
-            <!--Start Email Filed-->
-            <div class="form-group form-group-lg">
-                <label class="col-sm-2 control-label">Email</label>
-                <div class="col-sm-10 col-md-6">
-                    <input class="form-control" type="email" name="email">
-                </div>
-            </div>
-            <!--End Email Filed-->
-            <!--Start Full Name Filed-->
-            <div class="form-group form-group-lg">
-                <label class="col-sm-2 control-label">Full Name</label>
-                <div class="col-sm-10 col-md-6">
-                    <input class="form-control" type="text" name="full">
-                </div>
-            </div>
-            <!--End Full Name Filed-->
-            <!--Start Submit Filed-->
-            <div class="form-group form-group-lg">
-                <div class="col-sm-offset-2 col-sm-10">
-                    <input class="btn btn-primary" type="submit" value="Save">
-                </div>
-            </div>
-            <!--End Submit Field-->
+         <?php
+                $userid = (isset($_GET['userid']) && is_numeric($_GET['userid'])) ?  intval($_GET['userid']) : 0;
+                $stmt = $con->prepare("SELECT * FROM users WHERE UserId = $userid LIMIT 1");
+                $stmt->execute(array($userid));
+                $row = $stmt->fetch();
+                $count = $stmt->rowCount();
+                if($count > 0){   ?>
+                    <h1 class="text-center">Edit Member</h1>
+                    <form class="form-horizontal">
+                        <!--Start Username Filed-->
+                        <div class="form-group form-group-lg">
+                            <label class="col-sm-2 control-label">Username</label>
+                            <div class="col-sm-10 col-md-6">
+                                <input class="form-control" type="text" name="username" value="<?php echo $row['UserName']; ?>" autocomplete="off ">
+                            </div>
+                        </div>
+                        <!--End Username Filed-->
+                        <!--Start Password Filed-->
+                        <div class="form-group form-group-lg">
+                            <label class="col-sm-2 control-label">Password</label>
+                            <div class="col-sm-10 col-md-6">
+                                <input class="form-control" type="password" name="password"  autocomplete="new-password">
+                            </div>
+                        </div>
+                        <!--End Password Filed-->
+                        <!--Start Email Filed-->
+                        <div class="form-group form-group-lg">
+                            <label class="col-sm-2 control-label">Email</label>
+                            <div class="col-sm-10 col-md-6">
+                                <input class="form-control" type="email" value="<?php echo $row['Email']; ?>" name="email">
+                            </div>
+                        </div>
+                        <!--End Email Filed-->
+                        <!--Start Full Name Filed-->
+                        <div class="form-group form-group-lg">
+                            <label class="col-sm-2 control-label">Full Name</label>
+                            <div class="col-sm-10 col-md-6">
+                                <input class="form-control" type="text" value="<?php echo $row['FullName']; ?>" name="full">
+                            </div>
+                        </div>
+                        <!--End Full Name Filed-->
+                        <!--Start Submit Filed-->
+                        <div class="form-group form-group-lg">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <input class="btn btn-primary" type="submit" value="Save">
+                            </div>
+                        </div>
+                        <!--End Submit Field-->
+                    </form>
+                    <div class="container"></div>
 
-        </form>
-        <div class="container"></div>
-    <?php }
+    <?php } # Count's If
+                else {
+                    echo 'There\'s No Such Id';
+                }
+    }
     include_once $tpl . 'footer.php';
 } else {
     header('Location: index.php');
