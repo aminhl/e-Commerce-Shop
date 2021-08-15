28daa29175cd3cff9db12021673c99d18f7e975a
aminhl <amin.hlel@esprit.tn>
2021-08-15 17:49:11 +0100

    Categories - Create Insert Category Page

--- admin/categories.php
+++ admin/categories.php
@@ -7,7 +7,18 @@ if (isset($_SESSION['username'])){
     include_once 'init.php';
     $do = isset($_GET['do']) ? $_GET['do'] : 'Manage';
 
-    if ($do == 'Manage'){ ?>
+    if ($do == 'Manage'){
+
+    }
+    elseif($do == 'Edit'){
+
+    }
+
+    elseif ($do == 'Update'){
+
+    }
+
+    elseif($do == 'Add'){ ?>
         <h1 class="text-center">Add Category</h1>
         <div class="container">
             <form class="form-horizontal" action="?do=Insert" method="post">
@@ -39,10 +50,10 @@ if (isset($_SESSION['username'])){
                 <div class="form-group form-group-lg">
                     <label class="col-sm-2 control-label">Visibility</label>
                     <div class="col-sm-10 col-md-6">
-                       <div>
-                           <input id="visibility-yes" type="radio" name="visibility" value = "0" checked>
-                           <label for="visibility-yes">Yes</label>
-                       </div>
+                        <div>
+                            <input id="visibility-yes" type="radio" name="visibility" value = "0" checked>
+                            <label for="visibility-yes">Yes</label>
+                        </div>
                         <div>
                             <input id="visibility-no" type="radio" name="visibility" value = "1" >
                             <label for="visibility-no">No</label>
@@ -90,20 +101,49 @@ if (isset($_SESSION['username'])){
             </form>
         </div>
     <?php }
-    elseif($do == 'Edit'){
-
-    }
-
-    elseif ($do == 'Update'){
-
-    }
-
-    elseif($do == 'Add'){
-
-    }
 
     elseif ($do == 'Insert'){
+        if ($_SERVER['REQUEST_METHOD']=='POST'){
+            echo  '<h1 class="text-center">Insert Member</h1>';
+            echo '<div class="container">';
+            # Get Variables From The Form
+            $name = $_POST['name'];
+            $desc = $_POST['description'];
+            $order = $_POST['ordering'];
+            $visibility = $_POST['visibility'];
+            $comment = $_POST['commenting'];
+            $ads = $_POST['ads'];
 
+            $check = checkItem('Name','categories',$name);
+
+            if ($check == 1){
+                $theMsg = '<div class="alert alert-danger">This User Already Exist' . '</div>';
+                redirectHome($theMsg,'Previous');
+            }
+
+            else{
+                # Insert Data Base
+                $stmt = $con->prepare("INSERT INTO categories(Name,Description,Ordering,Visibility,Allow_Comment,Allow_Ads) VALUES(:name, :desc, :order, :visi, :comm, :ads)");
+                $stmt->execute(array(
+                    'name' => $name,
+                    'desc' => $desc,
+                    'order' => $order,
+                    'visi' => $visibility,
+                    'comm' => $comment,
+                    'ads' => $ads,
+                ));
+                $theMsg =  '<div class="alert alert-success">' . $stmt->rowCount() . ' Record Inserted </div>';
+                redirectHome($theMsg,'Previous');
+            }
+
+        }
+        else{
+            echo '<div class="container">';
+            $theMsg =  '<div class="alert alert-danger">U Can\'t Browse This Page Directly</div>';
+            redirectHome($theMsg);
+            echo '</div>';
+        }
+        echo '</div>';
     }
 
     elseif($do == 'Delete'){
