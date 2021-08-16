356e1981a94c37ae86bdb640faadc56be41300c7
aminhl <amin.hlel@esprit.tn>
2021-08-16 11:28:11 +0100

    Categories - Create Edit Page

--- admin/categories.php
+++ admin/categories.php
@@ -32,7 +32,7 @@ if (isset($_SESSION['username'])){
                    foreach ($cats as $cat){
                        echo '<div class="cat">';
                        echo '<div class="hidden-buttons">';
-                       echo '<a href="#" class="btn btn-xs btn-primary"><i class="fa fa-edit"></i> Edit</a>';
+                       echo '<a href="categories.php?do=Edit&catid='. $cat['ID'] . '" class="btn btn-xs btn-primary"><i class="fa fa-edit"></i> Edit</a>';
                        echo '<a href="#" class="btn btn-xs btn-danger"><i class="fa fa-close"></i> Delete</a>';
                        echo '</div>';
                        echo '<h3>'. $cat['Name'] . '</h3>';
@@ -48,8 +48,103 @@ if (isset($_SESSION['username'])){
             </div>
         </div>
     <?php }
-    elseif($do == 'Edit'){
+    elseif($do == 'Edit'){ ?>
+        <?php
+        $catid = (isset($_GET['catid']) && is_numeric($_GET['catid'])) ?  intval($_GET['catid']) : 0;
+        $stmt = $con->prepare("SELECT * FROM categories WHERE ID = ?");
+        $stmt->execute(array($catid));
+        $cat = $stmt->fetch();
+        $count = $stmt->rowCount();
+        if($count > 0){   ?>
+            <h1 class="text-center">Edit Category</h1>
+            <div class="container">
+                <form class="form-horizontal" action="?do=Update" method="post">
+                    <!--Start Name Filed-->
+                    <div class="form-group form-group-lg">
+                        <label class="col-sm-2 control-label">Name</label>
+                        <div class="col-sm-10 col-md-6">
+                            <input class="form-control" type="text" name="name" value="<?php echo $cat['Name']; ?>" required="required">
+                        </div>
+                    </div>
+                    <!--End Name Filed-->
+                    <!--Start Description Filed-->
+                    <div class="form-group form-group-lg">
+                        <label class="col-sm-2 control-label">Description</label>
+                        <div class="col-sm-10 col-md-6">
+                            <input class="form-control" type="text" name="description" value="<?php echo $cat['Description']; ?>">
+                        </div>
+                    </div>
+                    <!--End Description Filed-->
+                    <!--Start Ordering Filed-->
+                    <div class="form-group form-group-lg">
+                        <label class="col-sm-2 control-label">Ordering</label>
+                        <div class="col-sm-10 col-md-6">
+                            <input class="form-control" type="text"  name="ordering" value="<?php echo $cat['Ordering']; ?>">
+                        </div>
+                    </div>
+                    <!--End Ordering Filed-->
+                    <!--Start Visibility Filed-->
+                    <div class="form-group form-group-lg">
+                        <label class="col-sm-2 control-label">Visibility</label>
+                        <div class="col-sm-10 col-md-6">
+                            <div>
+                                <input id="visibility-yes" type="radio" name="visibility" value = "0" <?php if($cat['Visibility'] == 0) { echo 'checked';} ?>>
+                                <label for="visibility-yes">Yes</label>
+                            </div>
+                            <div>
+                                <input id="visibility-no" type="radio" name="visibility" value = "1" <?php if($cat['Visibility'] == 1) { echo 'checked';} ?>>
+                                <label for="visibility-no">No</label>
+                            </div>
+                        </div>
+                    </div>
+                    <!--End Visibility Filed-->
+                    <!--Start Commenting Filed-->
+                    <div class="form-group form-group-lg">
+                        <label class="col-sm-2 control-label">Allow Commenting</label>
+                        <div class="col-sm-10 col-md-6">
+                            <div>
+                                <input id="commenting-yes" type="radio" name="commenting" value = "0" <?php if($cat['Allow_Comment'] == 0) { echo 'checked';} ?>>
+                                <label for="commenting-yes">Yes</label>
+                            </div>
+                            <div>
+                                <input id="commenting-no" type="radio" name="commenting" value = "1" <?php if($cat['Allow_Comment'] == 1) { echo 'checked';} ?>>
+                                <label for="commenting-no">No</label>
+                            </div>
+                        </div>
+                    </div>
+                    <!--End Commenting Filed-->
+                    <!--Start Ads Filed-->
+                    <div class="form-group form-group-lg">
+                        <label class="col-sm-2 control-label">Allow Ads</label>
+                        <div class="col-sm-10 col-md-6">
+                            <div>
+                                <input id="ads-yes" type="radio" name="ads" value = "0" <?php if($cat['Allow_Ads'] == 0) { echo 'checked';} ?>>
+                                <label for="ads-yes">Yes</label>
+                            </div>
+                            <div>
+                                <input id="ads-no" type="radio" name="ads" value = "1" <?php if($cat['Allow_Ads'] == 1) { echo 'checked';} ?>>
+                                <label for="ads-no">No</label>
+                            </div>
+                        </div>
+                    </div>
+                    <!--End Ads Filed-->
+                    <!--Start Submit Filed-->
+                    <div class="form-group form-group-lg">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <input class="btn btn-primary" type="submit" value="Update">
+                        </div>
+                    </div>
+                    <!--End Submit Field-->
+                </form>
+            </div>
 
+        <?php } # Count's If
+        else {
+            echo '<div class="container">';
+            $theMsg =  '<div class="alert alert-danger">There\'s No Such Id </div>';
+            redirectHome($theMsg);
+            echo '</div>';
+        }
     }
 
     elseif ($do == 'Update'){
