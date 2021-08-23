84e9ac2feb3f262e2843537cd4c4eebd36d115be
aminhl <amin.hlel@esprit.tn>
2021-08-23 10:28:08 +0100

    Comments - Edit, Update, Delete & Approve

--- admin/comments.php
+++ admin/comments.php
@@ -32,9 +32,9 @@ if (isset($_SESSION['username'])){
                         echo '<td> ' . $row["Item_Name"] . '</td>';
                         echo '<td> ' . $row["Member"] . '</td>';
                         echo '<td>' . $row["comment_date"] . '</td>';
-                        echo '<td>   <a href="comments.php?do=Edit&comid='. $row["item_id"] .'" class="btn btn-success"><i class="fa fa-edit"></i> Edit</a> <a href="comments.php?do=Delete&comid='.  $row["item_id"] .'" class="btn btn-danger confirm"><i class="fa fa-close"></i> Delete</a>';
+                        echo '<td>   <a href="comments.php?do=Edit&comid='. $row["c_id"] .'" class="btn btn-success"><i class="fa fa-edit"></i> Edit</a> <a href="comments.php?do=Delete&comid='.  $row["c_id"] .'" class="btn btn-danger confirm"><i class="fa fa-close"></i> Delete</a>';
                         if ($row['status']==0){
-                            echo '<a href="comments.php?do=Approve&comid='.  $row["item_id"] .'" class="btn btn-info activate"><i class="fa fa-close"></i> Approve</a>';
+                            echo '<a href="comments.php?do=Approve&comid='.  $row["c_id"] .'" class="btn btn-info activate"><i class="fa fa-close"></i> Approve</a>';
                         }
                         echo  '</td>';
                         echo '</tr>';
@@ -46,17 +46,100 @@ if (isset($_SESSION['username'])){
         </div>
     <?php }
 
-    elseif ($do == 'Edit'){
+    elseif ($do == 'Edit'){ ?>
+        <?php
+                $comid = (isset($_GET['comid']) && is_numeric($_GET['comid'])) ?  intval($_GET['comid']) : 0;
+                $stmt = $con->prepare("SELECT * FROM comments WHERE c_id = ?");
+                $stmt->execute(array($comid));
+                $row = $stmt->fetch();
+                $count = $stmt->rowCount();
+                if($count > 0){   ?>
+                    <div class="container">
+                        <h1 class="text-center">Edit Comment</h1>
+                        <form class="form-horizontal" action="?do=Update" method="post">
+                            <input type="hidden" name="comid" value="<?php echo $comid;?>">
+                            <!--Start Comment Filed-->
+                            <div class="form-group form-group-lg">
+                                <label class="col-sm-2 control-label">Comment</label>
+                                <div class="col-sm-10 col-md-6">
+                                   <textarea class="form-control" name="comment"><?php echo $row['comment']; ?></textarea>
+                                </div>
+                            </div>
+                            <!--End Comment Filed-->
+                            <!--Start Submit Filed-->
+                            <div class="form-group form-group-lg">
+                                <div class="col-sm-offset-2 col-sm-10">
+                                    <input class="btn btn-primary" type="submit" value="Save">
+                                </div>
+                            </div>
+                            <!--End Submit Field-->
+                        </form>
+                    </div>
 
+                <?php } # Count's If
+                else {
+                    echo '<div class="container">';
+                    $theMsg =  '<div class="alert alert-danger">There\'s No Such Id </div>';
+                    redirectHome($theMsg);
+                    echo '</div>';
+                }
     }
     elseif ($do == 'Update'){
+        echo  '<h1 class="text-center">Update Comment</h1>';
+        echo '<div class="container">';
+        if ($_SERVER['REQUEST_METHOD']=='POST'){
+            # Get Variables From The Form
+            $id = $_POST['comid'];
+            $com = $_POST['comment'];
 
+            # Update Data Base
+            $stmt = $con->prepare("UPDATE comments SET comment = ?  WHERE c_id = ? ");
+            $stmt->execute(array($com,$id));
+            $theMsg = '<div class="alert alert-success">' . $stmt->rowCount() . ' Record Updated </div>';
+            redirectHome($theMsg,'Previous');
+        }
+        else{
+            $theMsg = '<div class="alert alert-danger">U Re Not Authorized To Be Here</div>';
+            redirectHome($theMsg);
+        }
+        echo '</div>';
     }
     elseif($do == 'Delete'){
+        echo  '<h1 class="text-center">Delete Comment</h1>';
+        echo '<div class="container">';
+        $comid = (isset($_GET['comid']) && is_numeric($_GET['comid'])) ?  intval($_GET['comid']) : 0;
+        $check = checkItem('c_id','comments',$comid);
 
+        if($check > 0){
+            $stmt = $con->prepare("DELETE FROM comments WHERE c_id = :comid");
+            $stmt->bindParam(":comid",$comid);
+            $stmt->execute();
+            $theMsg =  '<div class="alert alert-success">' . $stmt->rowCount() . ' Record Deleted </div>';
+            redirectHome($theMsg);
+        }
+        else{
+            $theMsg =  '<div class="alert alert-danger">This Id Doesn\'t Exist</div>';
+            redirectHome($theMsg);
+        }
+        echo '</div>';
     }
     elseif ($do == 'Approve'){
+        echo  '<h1 class="text-center">Approve Comment</h1>';
+        echo '<div class="container">';
+        $comid = (isset($_GET['comid']) && is_numeric($_GET['comid'])) ?  intval($_GET['comid']) : 0;
+        $check = checkItem('c_id','comments',$comid);
 
+        if($check > 0){
+            $stmt = $con->prepare("UPDATE comments SET status = 1 WHERE c_id = ?");
+            $stmt->execute(array($comid));
+            $theMsg =  '<div class="alert alert-success">' . $stmt->rowCount() . ' Record Updated </div>';
+            redirectHome($theMsg,'Previous');
+        }
+        else{
+            $theMsg =  '<div class="alert alert-danger">This Id Doesn\'t Exist</div>';
+            redirectHome($theMsg);
+        }
+        echo '</div>';
     }
     include_once $tpl . 'footer.php';
 }
